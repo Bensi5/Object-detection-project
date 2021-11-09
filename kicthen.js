b621e2154c4ba9510434292b51b347ef1350c086
Bensi5 <77843600+Bensi5@users.noreply.github.com>
2021-11-09 17:54:42 +0530

    Add files via upload

---
+++ kicthen.js
@@ -0,0 +1,27 @@
+img="";
+status="";
+function preload(){
+img=loadImage("k1.png");
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Object";
+    }
+    function draw(){
+    image(img,0,0,640,420);
+    }
+    function modelLoaded(){
+        console.log("Model is loaded");
+        status=true;
+        objectDetector.detect(img,gotResults);
+    }
+    function gotResults(results,error){
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log(results);
+        }
+    }
