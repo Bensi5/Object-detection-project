d78cb45e91e9247dd91cf6a3f9ffa219c87ea8d7
Bensi5 <77843600+Bensi5@users.noreply.github.com>
2021-11-12 17:50:07 +0530

    Add files via upload

--- kicthen.js
+++ kicthen.js
@@ -1,27 +1,40 @@
 img="";
 status="";
+object=[];
 function preload(){
 img=loadImage("k1.png");
 }
-function setup(){
-    canvas=createCanvas(640,420);
+function setup() {
+    canvas = createCanvas(640, 420);
     canvas.center();
-    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML="Status : Detecting Object";
-    }
-    function draw(){
-    image(img,0,0,640,420);
-    }
-    function modelLoaded(){
-        console.log("Model is loaded");
-        status=true;
-        objectDetector.detect(img,gotResults);
-    }
-    function gotResults(results,error){
-        if(error){
-            console.log(error);
-        }
-        else{
-            console.log(results);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        for (i = 0; i < object.length; i++) {
+            fill = "red";
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x+15, object[i].y+15);
+            noFill();
+            stroke("red");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
         }
     }
+}
+function modelLoaded() {
+    console.log("Model is loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        object = results;
+    }
+}
--- living.js
+++ living.js
@@ -1,27 +1,40 @@
-img="";
-status="";
-function preload(){
-img=loadImage("livingroom.jpg");
+img = "";
+status = "";
+object = [];
+function preload() {
+    img = loadImage("livingroom.jpg");
 }
-function setup(){
-canvas=createCanvas(640,420);
-canvas.center();
-objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-document.getElementById("status").innerHTML="Status : Detecting Object";
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
 }
-function draw(){
-image(img,0,0,640,420);
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        for (i = 0; i < object.length; i++) {
+            fill = "red";
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x+15, object[i].y+15);
+            noFill();
+            stroke("red");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
 }
-function modelLoaded(){
+function modelLoaded() {
     console.log("Model is loaded");
-    status=true;
-    objectDetector.detect(img,gotResults);
+    status = true;
+    objectDetector.detect(img, gotResults);
 }
-function gotResults(results,error){
-    if(error){
+function gotResults(error, results) {
+    if (error) {
         console.log(error);
     }
-    else{
+    else {
         console.log(results);
+        object = results;
     }
 }
